5e407d29daeab79062f55033528dc1fb35e3f614
elizabethgraham <elizabeth.graham2000@gmail.com>
2020-08-29 14:49:38 -0500

    Allowed for perpetual calculations. Added base error handling. Still need to validate that the user input is valid (int, float, arithmetic operator)

--- calculator.py
+++ calculator.py
@@ -1,18 +1,34 @@
 import operator
 
+# Dictionary of arithmetic operators and their respective functions
 ops = {'+': operator.add, '-': operator.sub, '*': operator.mul,
        '/': operator.truediv, '%': operator.mod, '^': operator.xor}
 
-
-def calculate():
-    first_num = input("First Number: ")
-    user_op = input("Operation: ")
-    sec_num = input("Second Number: ")
-
-    print(ops[user_op](int(first_num), int(sec_num)))
-
-
 print("Calculator Program")
 print("Use numerical characters and +, -, *, /, %, ^ operators.")
+
+
+def calculate():
+    try:
+        while True:
+            first_num = int(input("First Number: "))
+            user_op = input("Operation: ")
+            sec_num = int(input("Second Number: "))
+
+            # print the result of user selected operation of (1st number and 2nd number)
+            print(ops[user_op](first_num, sec_num))
+
+            again = input("Would you like to do another calculation? Press any key to continue or 'n' to quit")
+            if again.lower() == 'n':
+                break
+            else:
+                continue
+
+    # If the user presses Ctrl-C, end the script
+    except ZeroDivisionError or ValueError:
+        print("Zero Division Error or Value Error")
+    except:
+        print("stop testing me")
+
 
 calculate()
